090bb4b54275050ae94d420086daf0aed50e8aee
Erik Schmauss <erik.schmauss@intel.com>
2019-05-20 17:58:51 -0700

    remove useless html tags
    Signed-off-by: Erik Schmauss <erik.schmauss@intel.com>

--- scripts/do-pandoc-1.sh
+++ scripts/do-pandoc-1.sh
@@ -4,4 +4,7 @@ for file in ../frameMakerOutput/*.htm; do
     targetFilename=`basename -s .htm $file`.rst
     echo "Converting $file to $targetFilename"
     pandoc --from html --to rst -o $targetFilename $file
+    sed -i "/^.. raw:: html/d" $targetFilename
+    sed -i "/^   <\/div>/d" $targetFilename
+    sed -i "/^   <div>/d" $targetFilename
 done
