e625fe7207b550aadbc0a3cc5ddd65be2fdc497a
Erik Schmauss <erik.schmauss@intel.com>
2019-05-30 11:43:37 -0700

    Clean up sphinx files and run linkcheck during build process

--- scripts/convert-html.sh
+++ scripts/convert-html.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# clean sphinx files
+rm -f ../Makefile ../make.bat
+rm -rf ../source ../build
+
+# run conversion
 . rename-files-with-spaces.sh
 . do-pandoc-1.sh
 . gather-files.sh
+
+# run linkcheck
+pushd ..
+make linkcheck
+popd
